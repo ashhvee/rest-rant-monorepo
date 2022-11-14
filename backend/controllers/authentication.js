2315eb19abdb1ab11a036b85510508594f670b12
Ashley Vieira <ash.adr24212@gmail.com>
2022-11-14 18:07:24 -0500

    find user by email

--- backend/controllers/authentication.js
+++ backend/controllers/authentication.js
@@ -4,8 +4,14 @@ const bcrypt = require('bcrypt')
 
 const { User } = db
 
+  
 router.post('/', async (req, res) => {
-    console.log('IN HERE')
+
+    let user = await User.findOne({
+        where: { email: req.body.email }
+    })
+
+    console.log(user)
 })
 
 module.exports = router
