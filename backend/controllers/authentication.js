de9c866f2a398f3529122869f2a6f06d0e009d0c
Ashley Vieira <ash.adr24212@gmail.com>
2022-11-14 19:11:07 -0500

    reload pg w/o being logged out unless cookies deleted

--- backend/controllers/authentication.js
+++ backend/controllers/authentication.js
@@ -22,13 +22,24 @@ router.post('/', async (req, res) => {
     }
 })
 
+router.post('/super-important-route', async (req, res) => {
+    if(req.session.userId){
+        console.log('Do the really super important thing')
+        res.send('Done')
+    } else {
+        console.log('You are not authorized to do the super important thing')
+        res.send('Denied')
+    }
+})
+
+
 
 router.get('/profile', async (req, res) => {
-    console.log(req.session.userId)
+    // console.log(req.session.userId)
     try {
         let user = await User.findOne({
             where: {
-                userId: __
+                userId: req.session.userId
             }
         })
         res.json(user)
